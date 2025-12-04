7c6ed6a6e5f0aa25ec657e2db1317773dc3ac34c
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-04 15:44:29 +0100

    fix grammar

--- src/test/java/org/example/Utils/TestTest.java
+++ src/test/java/org/example/Utils/TestTest.java
@@ -1,12 +1,7 @@
 package org.example.Utils;
 
-import org.example.Main;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
