ff2a1b551fa8baf63e3959394df5534ea6448bf8
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-04 10:47:57 +0100

    add @Test for FizzBuzz

--- src/main/java/org/example/Utils/Test.java
+++ src/main/java/org/example/Utils/Test.java
@@ -12,10 +12,29 @@ package org.example.Utils;
  * - exclude previously added files from git changes through GitBash (git rm -r) and readd the files needed (git add .)
  */
 
-public class Test {
+public class Test
+{
+    public static String fizzBuzz(int number)
+    {
+        if (number % 3 == 0 && number % 5 == 0)
+        {
+            return "FizzBuzz";
 
-    public static String fizzBuzz(int number) {}
+        } else if (number % 3 == 0) {
+            return "Fizz";
 
+        }
+        else if (number % 5 == 0)
+        {
+            return "Buzz";
+        }
+
+        return "" + number;
+    }
 }
+
+
+
+
 
 
--- src/test/java/org/example/Utils/TestTest.java
+++ src/test/java/org/example/Utils/TestTest.java
@@ -1,12 +1,22 @@
 package org.example.Utils;
 
+import org.example.Main;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestTest {
 
+    // Red -> Green -> Refactor (Prinzip eines TDD)
     @Test
-    void fizzBuzz() {
+    void fizzBuzz_shouldReturnFizz_whenGivenNumberIs5() {
+
+        //GIVEN
+        String expected = "Fizz";
+        int number = 5;
+        //WHEN
+        String actual = org.example.Utils.Test.fizzBuzz(number);
+        //THEN
+        assertEquals(expected, actual);
     }
 }
