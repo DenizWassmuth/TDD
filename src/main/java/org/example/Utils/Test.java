ff0bf2c44dcce2eae8ea6a5a9ab83514a12aec88
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-04 10:09:37 +0100

    add git Repo

---
+++ src/main/java/org/example/Utils/Test.java
@@ -0,0 +1,17 @@
+package org.example.Utils;
+
+
+/**
+ *
+ * Test – kurze Beschreibung
+ * Author: Deniz Wassmuth
+ * Date: 04.12.2025
+ * <p>
+ * KEEP IN MIND:
+ * - check and/or change user and email through GitBash
+ * - add /target, *.iml and .idea to gitignore
+ * - exclude previously added files from git changes through GitBash (git rm -r) and readd the files needed (git add .)
+ */
+
+public class Test {
+}
---
+++ src/test/java/org/example/Utils/TestTest.java
@@ -0,0 +1,7 @@
+package org.example.Utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+
+}
